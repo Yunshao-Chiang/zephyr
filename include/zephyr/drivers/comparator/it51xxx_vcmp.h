021d4242fea7ea709ec6b2dd4a464fdeeb4880ab
Yunshao Chiang <Yunshao.Chiang@ite.com.tw>
2025-01-23 15:29:53 +0800

    drivers: comparator: add it51xxx_evb analog comparator driver
    Add analog comparator driver for ITE it51xxx chip.
    
    Change-Id: I6917d88fde7abd737b72200cbbbe3c4321bef8ce
    Signed-off-by: Yunshao Chiang <Yunshao.Chiang@ite.com.tw>

---
+++ include/zephyr/drivers/comparator/it51xxx_vcmp.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 ITE Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_COMPARATOR_IT51XXX_VCMP_H_
+#define ZEPHYR_INCLUDE_DRIVERS_COMPARATOR_IT51XXX_VCMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum comp_it51xxx_attribute {
+	COMPARATOR_ATTR_LOWER_VOLTAGE_THRESH,
+	COMPARATOR_ATTR_UPPER_VOLTAGE_THRESH
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_COMPARATOR_IT51XXX_VCMP_H_ */
